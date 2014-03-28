2862d60b0e2dbf4957f4ae243330d36f1982f4a3
David Matlack <matlackdavid@gmail.com>
2014-03-27 21:42:13 -0700

    x86: add syscall.{c,h} for arch-dependent syscall routines
    ... like return_from_syscall!

---
+++ kernel/arch/x86/inc/syscall.h
@@ -0,0 +1,9 @@
+/**
+ * @file arch/x86/syscall.h
+ */
+#ifndef __ARCH_X86_SYSCALL_H__
+#define __ARCH_X86_SYSCALL_H__
+
+void return_from_syscall(int ret);
+
+#endif /* !__ARCH_X86_SYSCALL_H__ */
--- kernel/arch/x86/proc.c
+++ kernel/arch/x86/proc.c
@@ -2,23 +2,7 @@
  * @file arch/x86/proc.c
  */
 #include <kernel/proc.h>
-#include <arch/cpu.h>
 
 void set_thread_regs(struct registers *regs) {
   CURRENT_THREAD->regs = regs;
-}
-
-//TODO: move function this somewhere else
-void return_from_syscall(int ret) {
-  CURRENT_THREAD->regs->eax = ret;
-
-  set_esp0(KSTACK_TOP);
-
-  //
-  // FIXME: this sets cr3 (tlb global flush) every time we return from
-  // a syscall. this is bad!
-  //
-  // (the fix is to just check in POP_REGISTERS if new_cr3 == current_cr3)
-  //
-  restore_registers(CURRENT_THREAD->regs);
 }
