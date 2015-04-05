9bc5c61f9e2ca2cac31b5b1c4cae4607f147e0cb
David Matlack <matlackdavid@gmail.com>
2015-04-04 23:05:27 -0700

    x86: fork: fix 80 char limit violations

--- kernel/arch/x86/fork.c
+++ kernel/arch/x86/fork.c
@@ -16,34 +16,38 @@ void fork_context(struct thread *new_thread)
 	uint32_t *ebp;
 
 	/*
-	 * Copy the current (aka parent) thread's syscall registers to the top of
-	 * the new thread's kernel stack. These registers will allow the new
-	 * thread to call return_from_syscall and get to userspace.
+	 * Copy the current (aka parent) thread's syscall registers to the
+	 * top of the new thread's kernel stack. These registers will allow
+	 * the new thread to call return_from_syscall and get to userspace.
 	 */
 	esp -= sizeof(struct registers) / sizeof(*esp);
 	new_thread->regs = (struct registers *) esp;
-	memcpy(new_thread->regs, CURRENT_THREAD->regs, sizeof(struct registers));
+	memcpy(new_thread->regs, CURRENT_THREAD->regs,
+	       sizeof(struct registers));
 
 	new_thread->regs->cr3 = new_cr3;
 
 	/*
-	 * Next put the argument for return_from_syscall. The child returns 0 from
-	 * fork.
+	 * Next put the argument for return_from_syscall. The
+	 * child returns 0 from fork.
 	 */
 	*(--esp) = 0;
 
 	/*
-	 * Next a fake return address for the return_from_syscall stack frame.
+	 * Next a fake return address for the return_from_syscall
+	 * stack frame.
 	 */
 	*(--esp) = 0xDEADBEEF;
 
 	/*
-	 * Next is the return address of __context_switch: return_from_syscall.
+	 * Next is the return address of __context_switch:
+	 * return_from_syscall.
 	 */
 	*(--esp) = (uint32_t) &return_from_syscall;
 
 	/*
-	 * Next is the old frame pointer for returning from __context_switch.
+	 * Next is the old frame pointer for returning from
+	 * __context_switch.
 	 */
 	--esp;
 	*(esp) = (uint32_t) (esp + 1);
@@ -66,6 +70,67 @@ void fork_context(struct thread *new_thread)
 	new_thread->context = cs_regs;
 }
 
+static void fork_pde(entry_t *from_pde, entry_t *to_pde, unsigned long virt)
+{
+	struct entry_table *to_pt;
+	struct entry_table *from_pt;
+	unsigned j;
+
+	from_pt = entry_pt(from_pde);
+
+	to_pt = new_entry_table();
+	if (!to_pt) {
+		panic("TODO: unmap all pages.");
+	}
+
+	/*
+	 * Copy the page directory entry.
+	 */
+	*to_pde = *from_pde;
+
+	/*
+	 * But use the address of the newly created page table.
+	 */
+	entry_set_addr(to_pde, __phys(to_pt));
+
+	for (j = 0; j < ENTRY_TABLE_SIZE; j++) {
+		entry_t *from_pte = from_pt->entries + j;
+		entry_t *to_pte = to_pt->entries + j;
+
+		/*
+		 * Copy the page table entry.
+		 */
+		*to_pte = *from_pte;
+
+		/*
+		 * If the entry is mapped to a physical page, increase the
+		 * reference count on that page by 1 as it is being used
+		 * in the new address space.
+		 */
+		if (entry_is_present(from_pte)) {
+			unsigned long phys = entry_phys(from_pte);
+
+			/*
+			 * Increase the reference counter on the page since
+			 * another page table now points to it.
+			 */
+			page_get(page_struct(phys));
+
+			/*
+			 * Mark the page readonly in both page tables so
+			 * both processes will page fault on a write and
+			 * we can copy the page.
+			 */
+			entry_set_readonly(from_pte);
+			entry_set_readonly(to_pte);
+
+			tlb_invalidate(virt, PAGE_SIZE);
+		}
+
+		virt += PAGE_SIZE;
+	}
+}
+
 int fork_address_space(struct entry_table *to_pd, struct entry_table *from_pd)
 {
 	unsigned i;
@@ -77,79 +142,25 @@ int fork_address_space(struct entry_table *to_pd, struct entry_table *from_pd)
 		entry_t *from_pde = from_pd->entries + i;
 		entry_t *to_pde = to_pd->entries + i;
 
-		/*
-		 * All processes share the same page tables to map the kernel. So
-		 * the only thing we need to copy for a kernel mapping is to copy
-		 * the page directory entries.
-		 */
-		if (kernel_address(virt)) {
-			*to_pde = *from_pde;
-		}
-		/*
-		 * If it's a user address, and the page directory entry is present, we
-		 * assume that it's pointing to a page table and we make a copy of that
-		 * page table instead.
-		 *
-		 * WARNING: using 4MB pages (PSE) will break this code.
-		 */
-		else if (entry_is_present(from_pde)) {
-			struct entry_table *to_pt;
-			struct entry_table *from_pt;
-			unsigned j;
-
-			from_pt = entry_pt(from_pde);
-
-			to_pt = new_entry_table();
-			if (!to_pt) {
-				panic("TODO: unmap all pages.");
-			}
-
+		if (kernel_address(virt))
 			/*
-			 * Copy the page directory entry.
+			 * All processes share the same page tables to map
+			 * the kernel. So the only thing we need to copy for
+			 * a kernel mapping is to copy the page directory
+			 * entries.
 			 */
 			*to_pde = *from_pde;
-
+		else if (entry_is_present(from_pde))
 			/*
-			 * But use the address of the newly created page table.
+			 * If it's a user address, and the page directory
+			 * entry is present, we assume that it's pointing
+			 * to a page table and we make a copy of that page
+			 * table instead.
+			 *
+			 * WARNING: using 4MB pages (PSE) will break this
+			 * code.
 			 */
-			entry_set_addr(to_pde, __phys(to_pt));
-
-			for (j = 0; j < ENTRY_TABLE_SIZE; j++) {
-				entry_t *from_pte = from_pt->entries + j;
-				entry_t *to_pte = to_pt->entries + j;
-
-				/*
-				 * Copy the page table entry.
-				 */
-				*to_pte = *from_pte;
-
-				/*
-				 * If the entry is mapped to a physical page, increase the reference
-				 * count on that page by 1 as it is being used in the new address
-				 * space.
-				 */
-				if (entry_is_present(from_pte)) {
-					unsigned long phys = entry_phys(from_pte);
-
-					/*
-					 * Increase the reference counter on the page since another page table
-					 * now points to it.
-					 */
-					page_get(page_struct(phys));
-
-					/*
-					 * Mark the page readonly in both page tables so both processes will 
-					 * page fault on a write and we can copy the page.
-					 */
-					entry_set_readonly(from_pte);
-					entry_set_readonly(to_pte);
-
-					tlb_invalidate(virt, PAGE_SIZE);
-				}
-
-				virt += PAGE_SIZE;
-			}
-		}
+			fork_pde(from_pde, to_pde, virt);
 	}
 
 	return 0;
