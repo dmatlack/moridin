a3fdaff3fb3c9dc70831dd23afb725a43c64c235
David Matlack <matlackdavid@gmail.com>
2015-04-19 19:05:47 -0700

    irq: protect irq handlers list with spinlock

--- kernel/kernel/irq.c
+++ kernel/kernel/irq.c
@@ -6,6 +6,7 @@
  */
 #include <kernel/irq.h>
 #include <kernel/sched.h>
+#include <kernel/spinlock.h>
 #include <mm/kmalloc.h>
 
 #include <arch/atomic.h>
@@ -19,6 +20,7 @@
 #include <string.h>
 
 list_typedef(struct irq_handler) irq_handler_list_t;
+struct spinlock lock = INITIALIZED_SPINLOCK;
 
 struct irq_desc {
 	irq_handler_list_t handlers;
@@ -41,15 +43,21 @@ void kernel_irq_handler(int irq)
 	struct irq_desc *desc = irq_descs + irq;
 	struct irq_context context  = { .irq = irq };
 	struct irq_handler *handler;
+	unsigned long flags;
+
+	spin_lock_irq(&lock, &flags);
 
 	if (list_empty(&desc->handlers))
-		return;
+		goto out;
 
 	atomic_add(&desc->count, 1);
 
 	list_foreach(handler, &desc->handlers, link) {
 		handler->f(&context);
 	}
+
+out:
+	spin_unlock_irq(&lock, flags);
 }
 
 void irq_exit(void)
@@ -60,39 +68,16 @@ void irq_exit(void)
 int register_irq(int irq, struct irq_handler *new_handler)
 {
 	struct irq_desc *desc = irq_descs + irq;
+	unsigned long flags;
 
 	ASSERT_GREATEREQ(irq, 0);
 	ASSERT_LESS(irq, MAX_NUM_IRQS);
 
+	spin_lock_irq(&lock, &flags);
+
 	list_insert_tail(&desc->handlers, new_handler, link);
 
+	spin_unlock_irq(&lock, flags);
+
 	return 0;
-}
-
-/**
- * @brief Print bar of text accross a row of the console with some useful
- * information about IRQs.
- *
- * @param bar_row The row to draw the bar.
- */
-void irq_status_bar(int bar_row)
-{
-	int old_row, old_col;
-	char old_color;
-	int i;
-
-	vga_get_cursor(&old_row, &old_col);
-	old_color = vga_get_color();
-
-	vga_set_color(VGA_COLOR(VGA_WHITE, VGA_BLUE));
-
-	vga_set_cursor(bar_row, 0);
-	kprintf("IRQs: ");
-	for (i = 0; i < MAX_NUM_IRQS; i++) {
-		if (irq_descs[i].count > 0)
-			kprintf("%d:%d ", i, irq_descs[i].count);
-	}
-
-	vga_set_cursor(old_row, old_col);
-	vga_set_color(old_color);
 }
