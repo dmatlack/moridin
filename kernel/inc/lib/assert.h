944b23dc001107f4f231a065c419165be0ce2194
David Matlack <matlackdavid@gmail.com>
2015-04-29 21:59:44 -0700

    assert: cleanup assertion code

--- kernel/inc/lib/assert.h
+++ kernel/inc/lib/assert.h
@@ -10,54 +10,45 @@
 #include <kernel/log.h>
 #include <arch/irq.h>
 
-#define panic(_fmt, ...)						      \
-	do {								      \
-		disable_irqs();						      \
-		FATAL("[%s:%d %s()] "_fmt,				      \
-		      __FILE__, __LINE__, __func__, ##__VA_ARGS__);	      \
-		backtrace();						      \
-		while (1) continue;					      \
-	} while (0)
+#define panic(_fmt, ...) do {						\
+	disable_irqs();							\
+									\
+	FATAL("[%s:%d %s()] "_fmt,					\
+	      __FILE__, __LINE__, __func__, ##__VA_ARGS__);		\
+									\
+	backtrace();							\
+	while (1) continue;						\
+} while (0)
 
-#define assert(expression) \
-	do { \
-		if (!(expression)) { \
-			panic("failed assertion `%s'", #expression); \
-		} \
-	} while (0)
+#define assert(expression) do {						\
+	signed long __condition = (signed long) (expression);		\
+									\
+	if (__condition)						\
+		break;							\
+									\
+	panic("Failed Assert: %s [%ld]",				\
+	      #expression, __condition);				\
+} while (0)
 
 #define ASSERT(expression) assert(expression)
-
 #define ASSERT_NOT_NULL(expression) ASSERT(NULL != (expression))
 
-#define ASSERT_FUNCTION_PROTOTYPE(_name)\
-	void _name(const char *file, const char *func, int line,\
-			int left, const char *leftstr,\
-			int right, const char *rightstr)
+#define ASSERT_BINARY_OPERATOR(_left, _op, _right) do {			\
+	signed long __left = (signed long) (_left);			\
+	signed long __right = (signed long) (_right);			\
+									\
+	if (__left _op __right)						\
+		break;							\
+									\
+	panic("Failed Assert: %s [%ld] %s %s [%ld]",			\
+	      #_left, __left, #_op, #_right, __right);			\
+} while (0)
 
-ASSERT_FUNCTION_PROTOTYPE(__assert_equals);
-ASSERT_FUNCTION_PROTOTYPE(__assert_notequals);
-ASSERT_FUNCTION_PROTOTYPE(__assert_less);
-ASSERT_FUNCTION_PROTOTYPE(__assert_lesseq);
-ASSERT_FUNCTION_PROTOTYPE(__assert_greater);
-ASSERT_FUNCTION_PROTOTYPE(__assert_greatereq);
-
-#define ASSERT_EQUALS(left, right)\
-	__assert_equals(__FILE__, __func__, __LINE__, (int)left, #left, (int)right, #right)
-
-#define ASSERT_NOTEQUALS(left, right)\
-	__assert_notequals(__FILE__, __func__, __LINE__, (int)left, #left, (int)right, #right)
-
-#define ASSERT_LESS(left, right)\
-	__assert_less(__FILE__, __func__, __LINE__, (int)left, #left, (int)right, #right)
-
-#define ASSERT_LESSEQ(left, right)\
-	__assert_lesseq(__FILE__, __func__, __LINE__, (int)left, #left, (int)right, #right)
-
-#define ASSERT_GREATER(left, right)\
-	__assert_greater(__FILE__, __func__, __LINE__, (int)left, #left, (int)right, #right)
-
-#define ASSERT_GREATEREQ(left, right)\
-	__assert_greatereq(__FILE__, __func__, __LINE__, (int)left, #left, (int)right, #right)
+#define ASSERT_EQUALS(left, right) ASSERT_BINARY_OPERATOR(left, ==, right)
+#define ASSERT_NOTEQUALS(left, right) ASSERT_BINARY_OPERATOR(left, !=, right)
+#define ASSERT_LESS(left, right) ASSERT_BINARY_OPERATOR(left, <, right)
+#define ASSERT_LESSEQ(left, right) ASSERT_BINARY_OPERATOR(left, <=, right)
+#define ASSERT_GREATER(left, right) ASSERT_BINARY_OPERATOR(left, >, right)
+#define ASSERT_GREATEREQ(left, right) ASSERT_BINARY_OPERATOR(left, >=, right)
 
 #endif /* _ASSERT_H_ */
